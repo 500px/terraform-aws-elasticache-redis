034e9f8aa240fc3944631a737a1d738e48b9a16a
x041 <x041@users.noreply.github.com>
2019-02-23 18:34:26 -0500

    workaround silly bug preventing non-encrypted redis clusters

--- main.tf
+++ main.tf
@@ -48,10 +48,37 @@ resource "aws_elasticache_parameter_group" "default" {
   parameter = "${var.parameter}"
 }
 
-resource "aws_elasticache_replication_group" "default" {
-  count = "${var.enabled == "true" ? 1 : 0}"
+# workaround silly terraform bug, see issue #35 in upstream repo
+resource "aws_elasticache_replication_group" "encrypt" {
+  count = "${var.enabled == "true" && var.transit_encryption_enabled == "true" ? 1 : 0}"
 
   auth_token                    = "${var.auth_token}"
+  replication_group_id          = "${var.replication_group_id == "" ? module.label.id : var.replication_group_id}"
+  replication_group_description = "${module.label.id}"
+  node_type                     = "${var.instance_type}"
+  number_cache_clusters         = "${var.cluster_size}"
+  port                          = "${var.port}"
+  parameter_group_name          = "${aws_elasticache_parameter_group.default.name}"
+  availability_zones            = ["${slice(var.availability_zones, 0, var.cluster_size)}"]
+  automatic_failover_enabled    = "${var.automatic_failover}"
+  subnet_group_name             = "${aws_elasticache_subnet_group.default.name}"
+  security_group_ids            = ["${aws_security_group.default.id}"]
+  maintenance_window            = "${var.maintenance_window}"
+  notification_topic_arn        = "${var.notification_topic_arn}"
+  engine_version                = "${var.engine_version}"
+  at_rest_encryption_enabled    = "${var.at_rest_encryption_enabled}"
+  transit_encryption_enabled    = "${var.transit_encryption_enabled}"
+  snapshot_window               = "${var.snapshot_window}"
+  snapshot_retention_limit      = "${var.snapshot_retention_limit}"
+  snapshot_arns                 = "${var.snapshot_arns}"
+  snapshot_name                 = "${var.snapshot_name}"
+
+  tags = "${module.label.tags}"
+}
+
+resource "aws_elasticache_replication_group" "default" {
+  count = "${var.enabled == "true" && var.transit_encryption_enabled == "false" ? 1 : 0}"
+
   replication_group_id          = "${var.replication_group_id == "" ? module.label.id : var.replication_group_id}"
   replication_group_description = "${module.label.id}"
   node_type                     = "${var.instance_type}"
