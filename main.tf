5e6f0c178e12f22c6ce0a6a5eb3e3d0e23d4e588
Matt Stobo <mstobo@500px.com>
2019-04-05 11:33:56 -0400

    Prefix friendly names

--- main.tf
+++ main.tf
@@ -153,7 +153,7 @@ module "dns" {
   source    = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-hostname.git?ref=tags/0.2.1"
   enabled   = "${var.enabled == "true" && length(var.zone_id) > 0 ? "true" : "false"}"
   namespace = "${var.namespace}"
-  name      = "${var.name}"
+  name      = "redis-${var.name}"
   stage     = "${var.stage}"
   ttl       = 60
   zone_id   = "${var.zone_id}"
